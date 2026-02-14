6f2c6ee976a5314575ab7e4b232cc1772bee96b5
kinggizzardandtheinstallationwizard <245943325+kinggizzardandtheinstallationwizard@users.noreply.github.com>
2026-02-14 04:04:53 -0600

    fix formatting, add missing endl, fix return value

--- supportticket.cpp
+++ supportticket.cpp
@@ -57,7 +57,10 @@ class Node {
         if (head == nullptr) {return false; }
         if (head->id == searchid) {
             cout << "Ticket Found:" << endl;
-            cout << "TicketID: " << head->id << " Customer Name: " << head->name << " Issue: " << head->issue << endl; return true;
+            cout << "TicketID: " << head->id <<
+		    " Customer Name: " << head->name <<
+		    " Issue: " << head->issue << endl;
+	    return true;
         }
         while (head != tail) {
             if (head->id != searchid && tail->id != searchid) {
@@ -65,12 +68,15 @@ class Node {
                 tail = tail->prevptr;
             } else if (head->id == searchid) {
                cout << "Ticket Found:" << endl;
-              cout << "TicketID: " << head->id << " Customer Name: " << head->name << " Issue: " << head->issue << endl;
+               cout << "TicketID: " << head->id
+		   << " Customer Name: " << head->name
+		   << " Issue: " << head->issue << endl;
               return true;
             } else if (tail->id == searchid) {
                  cout << "Ticket Found:" << endl;
-
-                 cout << "TicketID: " << tail->id << " Customer Name: " << tail->name << " Issue: " << tail->issue << endl;
+                 cout << "TicketID: " << tail->id
+		      << " Customer Name: " << tail->name
+		      << " Issue: " << tail->issue << endl;
               return true;
             }
         }
@@ -86,7 +92,9 @@ class Node {
         }
         cout << "All Pending Tickets in Pool: " << endl;
         while (tail != nullptr){
-            cout << "TicketID: " << tail->id << " Customer Name: " << tail->name << " Issue: " << tail->issue << endl;
+            cout << "TicketID: " << tail->id
+		 << " Customer Name: " << tail->name
+		 << " Issue: " << tail->issue << endl;
             tail = tail->prevptr;
         }
        
@@ -95,14 +103,21 @@ class Node {
     // test to reverse ticket queue
    void reverseTicketQueue(Node* tail) {
         while (tail != nullptr){
-            cout << "TicketID: " << tail->id << " Customer Name: " << tail->name << " Issue: " << tail->issue << endl;
+            cout << "TicketID: " << tail->id
+		 << " Customer Name: " << tail->name
+		 << " Issue: " << tail->issue << endl;
             tail = tail->prevptr;
         }
     }
 
     Node* serveTicket(Node* tail) {
-        if (tail == nullptr) {cout << "No Ticket to be served" << endl; return nullptr;}
-       cout << "TicketID: " << tail->id << " Customer Name: " << tail->name << " Issue: " << tail->issue << endl;
+       if (tail == nullptr) {
+		cout << "No Ticket to be served" << endl;
+		return nullptr;
+       }
+       cout << "TicketID: " << tail->id
+	    << " Customer Name: " << tail->name
+	    << " Issue: " << tail->issue << endl;
        Node* temp = tail->prevptr;
        delete tail;
        temp->nextptr = nullptr;
@@ -111,73 +126,74 @@ class Node {
 
 
     void peekTicket(Node* tail) {
-        cout << "TicketID: " << tail->id << " Customer Name: " << tail->name << " Issue: " << tail->issue << endl;
+        cout << "TicketID: " << tail->id
+	     << " Customer Name: " << tail->name
+	     << " Issue: " << tail->issue << endl;
     }
 
 
 
 
     
-int main() {
-bool systemOn = true;
-Node* head = nullptr;
-Node* tail = nullptr;
-while (systemOn) {
-    int choice;
-    cout << "--- Customer Support Ticket System ---" << endl;
-    cout << "1. Add a Ticket" << endl;
-    cout << "2. Serve a Ticket" << endl;
-    cout << "3. Peek Next Ticket" << endl;
-    cout << "4. Search Ticket by ID" << endl;
-    cout << "5. Display All Tickets" << endl;
-    cout << "6. Exit" << endl;
-    cout << "Enter your choice: ";
-    cin >> choice;
-    switch (choice) {
-        case 1: {
-        int id = 0;
-        string name = "";
-        string issue;
-        cout << "Enter Ticket ID:";
-        cin >> id;
-        cout << "Enter Customer Name:";
-        cin >> name;
-        cout << "Enter Issue Description:";
-        cin.ignore();
-        getline(cin, issue);
-        Node* newTicket = new Node(id, name, issue);
-        if (searchForTicket(newTicket->id, head, tail) == false) {
-        head = addTicketToQueue(newTicket, head);
-        tail = assignTail(tail, head);
-        cout << "Ticket added successfully to the pool." << endl;
-        } else {
-         cout << "Ticket already is queued" << endl;
-        }
-        break;
-    }
-        case 2: 
-        cout << "Serving Ticket";
-        tail = serveTicket(tail);
-        break;
-        case 3: 
-        cout << "Next Ticket to be Served:";
-         peekTicket(tail);
-        break;
-        case 4: { 
-        int id;
-        cout << "Enter TicketID to search:";
-        cin >> id;
-        searchForTicket(id, head, tail);
-        }
-        break;
-        case 5: 
-         traverseTicketQueue(head, tail); 
-        break;
-        case 6: 
-        cout << "Exiting System. Goodbye!";
-        systemOn = false;
-        break;
-    }
-}
-    return 1;
+int main(void) {
+	bool systemOn = true;
+	Node* head = nullptr;
+	Node* tail = nullptr;
+	while (systemOn) {
+		int choice;
+		cout << "--- Customer Support Ticket System ---" << endl;
+		cout << "1. Add a Ticket" << endl;
+		cout << "2. Serve a Ticket" << endl;
+		cout << "3. Peek Next Ticket" << endl;
+		cout << "4. Search Ticket by ID" << endl;
+		cout << "5. Display All Tickets" << endl;
+		cout << "6. Exit" << endl;
+		cout << "Enter your choice: ";
+		cin >> choice;
+		switch (choice) {
+			case 1: {
+				int id = 0;
+				string name = "";
+				string issue;
+				cout << "Enter Ticket ID:";
+				cin >> id;
+				cout << "Enter Customer Name:";
+				cin >> name;
+				cout << "Enter Issue Description:";
+				cin.ignore();
+				getline(cin, issue);
+				Node* newTicket = new Node(id, name, issue);
+				if (searchForTicket(newTicket->id, head, tail) == false) {
+					head = addTicketToQueue(newTicket, head);
+					tail = assignTail(tail, head);
+					cout << "Ticket added successfully to the pool." << endl;
+				} else {
+					cout << "Ticket already is queued" << endl;
+				}
+				break;
+			}
+			case 2: 
+				cout << "Serving Ticket";
+				tail = serveTicket(tail);
+				break;
+			case 3: 
+				cout << "Next Ticket to be Served:";
+				peekTicket(tail);
+				break;
+			case 4:
+				int id;
+				cout << "Enter TicketID to search:";
+				cin >> id;
+				searchForTicket(id, head, tail);
+				break;
+			case 5: 
+				traverseTicketQueue(head, tail); 
+				break;
+			case 6: 
+				cout << "Exiting System. Goodbye!" << endl;
+				systemOn = false;
+				break;
+		}
+	}
+	return 0;
 }
